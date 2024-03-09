bbc7abf29c8269ba90d4373a27d80a31fa426cb2
nikiitasharrma <nikita16301@gmail.com>
2024-03-09 12:47:37 +0530

    Project setup and added required dependencies

---
+++ src/test/java/com/nikita/projects/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
