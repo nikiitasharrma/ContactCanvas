311c6706e56621c4b02d748725ea1ca6cc51e59d
nikiitasharrma <nikita16301@gmail.com>
2024-03-11 12:57:52 +0530

    Process code for user signup with message

--- src/main/java/com/nikita/projects/controllers/MainController.java
+++ src/main/java/com/nikita/projects/controllers/MainController.java
@@ -1,48 +1,70 @@
 package com.nikita.projects.controllers;
 
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//import com.nikita.projects.UserRepository;
+import com.nikita.projects.entities.User;
+import com.nikita.projects.helper.Message;
 
-@RestController
+import jakarta.servlet.http.HttpSession;
+
+import com.nikita.projects.UserRepository;
+
+@Controller
 public class MainController {
-	
-//	@Autowired
-//	private UserRepository userRepo;
-	
+
+	@Autowired
+	private UserRepository userRepo;
+
 	@GetMapping("/")
-	public ModelAndView homeView() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("title","Home-Smart Contact Manager");
-		mv.setViewName("home");
-		return mv;
-	}
-	
-	@GetMapping("/about")
-	public ModelAndView aboutView() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("title","About-Smart Contact Manager");
-		mv.setViewName("about");
-		return mv;
-	}
-	
-	@GetMapping("/signup")
-	public ModelAndView signupView() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("title","Register-Smart Contact Manager");
-		mv.setViewName("signup");
-		return mv;
+	public String homeView(Model m) {
+		m.addAttribute("title", "Home-Smart Contact Manager");
+		return "home";
 	}
 
-//	public UserRepository getUserRepo() {
-//		return userRepo;
-//	}
-//
-//	public void setUserRepo(UserRepository userRepo) {
-//		this.userRepo = userRepo;
-//	}
+	@GetMapping("/about")
+	public String aboutView(Model m) {
+		m.addAttribute("title", "About-Smart Contact Manager");
+		return "about";
+	}
+
+	@GetMapping("/signup")
+	public String signupView(Model m) {
+		m.addAttribute("title", "Register-Smart Contact Manager");
+		m.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@PostMapping("/processSignup")
+	public String processSignup(@ModelAttribute("user") User user,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model m,
+			HttpSession session) {
+
+		try {
+			if (!agreement) {
+				throw new Exception("Please accept the terms and conditions to proceed with the registration");
+			}
+			User resultUser = userRepo.save(user);
+			System.out.println(resultUser + " " + agreement);
+			session.setAttribute("message", new Message("Successfully registered!", "alert-success"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("message", new Message("Something went wrong! " + e.getMessage(), "alert-danger"));
+		}
+		return "signup";
+	}
+
+	public UserRepository getUserRepo() {
+		return userRepo;
+	}
+
+	public void setUserRepo(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
 
 }
