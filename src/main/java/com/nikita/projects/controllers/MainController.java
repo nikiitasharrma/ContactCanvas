0042869e5480c3c64132c796ccb27acc8fdee085
nikiitasharrma <nikita16301@gmail.com>
2024-03-11 10:20:21 +0530

    designed home page

--- src/main/java/com/nikita/projects/controllers/MainController.java
+++ src/main/java/com/nikita/projects/controllers/MainController.java
@@ -1,60 +1,40 @@
 package com.nikita.projects.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.nikita.projects.UserRepository;
-import com.nikita.projects.entities.Contact;
-import com.nikita.projects.entities.User;
+//import com.nikita.projects.UserRepository;
 
 @RestController
 public class MainController {
 	
-	@Autowired
-	private UserRepository userRepo;
+//	@Autowired
+//	private UserRepository userRepo;
 	
-	@GetMapping("/test")
-	public String testJPA() {
-		User user = new User();
-		user.setName("Nikita");
-		user.setEmail("nikita@example.com");
-		
-		Contact c1 = new Contact();
-		c1.setContactNo("12345");
-		c1.setName("Abc");
-		c1.setUser(user);
-		
-		Contact c2 = new Contact();
-		c2.setContactNo("67890");
-		c2.setName("Xyz");
-		c2.setUser(user);
-		
-		List<Contact> contacts = List.of(c1, c2);
-		user.setContacts(contacts);
-		
-		userRepo.save(user);
-		
-		return "Test Successful!";
-	}
-	
-	@GetMapping("/home")
+	@GetMapping("/")
 	public ModelAndView homeView() {
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("title","Smart Contact Manager");
+		mv.addObject("title","Home-Smart Contact Manager");
 		mv.setViewName("home");
 		return mv;
 	}
-
-	public UserRepository getUserRepo() {
-		return userRepo;
+	
+	@GetMapping("/about")
+	public ModelAndView aboutView() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("title","About-Smart Contact Manager");
+		mv.setViewName("about");
+		return mv;
 	}
 
-	public void setUserRepo(UserRepository userRepo) {
-		this.userRepo = userRepo;
-	}
+//	public UserRepository getUserRepo() {
+//		return userRepo;
+//	}
+//
+//	public void setUserRepo(UserRepository userRepo) {
+//		this.userRepo = userRepo;
+//	}
 
 }
